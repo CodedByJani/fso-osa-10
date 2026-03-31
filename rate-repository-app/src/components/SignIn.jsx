220ce5a7757ed0917cb089e2ea23e1df328a7ff6
Jani Piirainen <piirainen.jani98@gmail.com>
2026-03-31 09:59:53 +0300

    Exercise 10.8 done

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,57 @@
+import { View, StyleSheet, TextInput, Button } from "react-native";
+import { Formik } from "formik";
 import Text from "./Text";
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: "#fff",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+});
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={onSubmit}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <>
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+              value={values.username}
+              onChangeText={handleChange("username")}
+              onBlur={handleBlur("username")}
+            />
+
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              value={values.password}
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              secureTextEntry
+            />
+
+            <Button title="Sign in" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
